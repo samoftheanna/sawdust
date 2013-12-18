8df9c7fe0e5e24326dc31f9bb6d82d307781d8ce
joanna <jsamuelson@ldschurch.org>
2013-12-18 12:06:38 -0700

    a starter js

---
+++ jo.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {    
+                var canvas = document.getElementById("myCanvas");  
+                var ctx = canvas.getContext("2d");
+                ctx.fillRect(50, 50, 100, 100);  
+				ctx.fillStyle = "rgb(255, 0, 0)";  
+				ctx.fillRect(200, 50, 100, 100);
+                ctx.strokeStyle = "rgb(255, 0, 0)";
+                ctx.lineWidth = 20;  
+				ctx.beginPath();  
+				ctx.moveTo(50, 50);  
+				ctx.lineTo(50, 250);  
+				ctx.lineTo(250, 250);  
+				ctx.closePath();  
+				ctx.stroke();  
+            });
